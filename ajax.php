f554ae7601fc247d3078c7135528cbe64b2b8f9f
yang20029798 <ymin.fahao@foxmail.com>
2015-04-20 19:02:14 +0800

    ajax

---
+++ ajax.php
@@ -0,0 +1,14 @@
+<?php
+    header("Content-Type: text/html; charset=utf-8");
+
+    
+    	 $arr=array(
+            'name'=>'阿斯顿发生',
+            'age'=>'25'
+    	 	);
+
+    	 echo json_encode($arr);
+    
+
+
+?>
